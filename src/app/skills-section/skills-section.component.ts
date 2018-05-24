623c54680ecdead53585a21cfb53200f3734ece8
Linus Castelino <linus.castelino@toysrus.com>
2018-05-24 21:19:28 +0530

    Used ngFor in skills component

--- src/app/skills-section/skills-section.component.ts
+++ src/app/skills-section/skills-section.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Skill } from '../model/Skill';
 
 @Component({
   selector: 'app-skills-section',
   templateUrl: './skills-section.component.html',
   styleUrls: ['./skills-section.component.css']
 })
+
 export class SkillsSectionComponent implements OnInit {
 
-  constructor() { }
+  private skills: Skill[];
+
+  constructor() { 
+    this.skills = [];
+    this.skills.push({skillTitle:'Java Programming', level:'Expert'});
+    this.skills.push({skillTitle:'Oracle Web Commerce', level:'Expert'});
+    this.skills.push({skillTitle:'SQL', level:'Expert'});
+    this.skills.push({skillTitle:'Shell Scripting', level:'Proficient'});
+    this.skills.push({skillTitle:'JBoss', level:'Proficient'});
+    this.skills.push({skillTitle:'JavaScript, jQuery', level:'Intermediate'});
+    this.skills.push({skillTitle:'REST/SOAP', level:'Intermediate'});
+    this.skills.push({skillTitle:'ANT', level:'Intermediate'});
+  }
 
   ngOnInit() {
+    
   }
 
 }
