2b1928ecd42d2cf8c83f8d33e2a33c96b2e28a77
Linus Castelino <castelinolinus@gmail.com>
2019-12-25 13:52:57 -0700

    Completed ProfExp section

--- src/app/prof-exp-section/prof-exp-section.component.ts
+++ src/app/prof-exp-section/prof-exp-section.component.ts
@@ -35,156 +35,72 @@ export class ProfExpSectionComponent implements OnInit {
         eventLogoURL: '../../assets/images/TCS-logo.png',
         eventHeader:{
           organization: 'Tata Consultancy Services Pvt. Ltd.',
-          designation: 'Full-Stack Developer',
+          designation: 'Systems Engineer (Full-Stack Developer)',
           role: 'A.T.G. Developer, E-Commerce - Retail & C.P.G.',
           period: 'Jan\'16 - Jul\'18'
         },
         event:{
           details: [
-            'Worked as an Oracle A.T.G. Web Commerce developer, implementing software modules for the U.K. and Poland market E-Commerce webstores of ToysRUs.',
+            'Worked as an Oracle A.T.G. Web Commerce developer, implementing software modules for the U.K. and Poland market ' + 
+            'E-Commerce webstores of ToysRUs.',
             // 'Click <a href="#">here</a> for detailed responsibilities.',
-            'Presented with the <a href="https://drive.google.com/file/d/0B40x1VYdbWc8WGxkQXNkOC11aFE/view?usp=sharing" ' +
-            'target="_blank">\'iONCommitment\'</a> award acknowledging the contributions made towards the project.'
+            
+            // 'Presented with the <a href="https://drive.google.com/file/d/0B40x1VYdbWc8WGxkQXNkOC11aFE/view?usp=sharing" ' +
+            // 'target="_blank">\'iONCommitment\'</a> award acknowledging the contributions made towards the project.',
+            // 'Participated as a contributor in a crowd sourcing program for CodeVita\'16, a hackathon organized by T.C.S. annually, by ' +
+            // 'creating questions/problems, optimal solutions and test cases. These questions were presented to the participants as ' +
+            // 'challenges.',
+            // 'Attained the 7th position among 100+ participants considering the number and the complexity of the selected questions.',
+            // 'Received a <a href="https://drive.google.com/file/d/0B40x1VYdbWc8RTduS0NDcEVDeUE/view?usp=sharing" target="_blank">\'Special ' +
+            // 'Initiative\'</a> award for the same.',
+            // 'Appointed as a Learning Enabler for a period of 1 month to assist the I.L.P. faculty in training new trainees at the I.L.P. ' +
+            // 'centre.',
+            // 'Responsibilities involved conducting sessions on software development life cycle and specific programming modules, helping ' +
+            // 'trainees with difficulties in project assignments, and evaluating periodic assessments.',
+            // 'Offered the <a href="https://drive.google.com/file/d/0B40x1VYdbWc8S2pYQkpHZHBwN00/view?usp=sharing" target="_blank" > ' +
+            // '\'Champions of I.L.P.\'</a> award.'
           ]
         }
       }
     );
 
-    // this.profExpEvents.unshift(
-    //   {
-    //     eventLogoURL: '',
-    //     eventHeader:{
-    //       organization: '',
-    //       designation: '',
-    //       role: '',
-    //       period: ''
-    //     },
-    //     event:{
-    //       details: []
-    //     }
-    //   }
-    // );
+    this.profExpEvents.unshift(
+      {
+        eventLogoURL: '../../assets/images/Salesforce-logo.png',
+        eventHeader:{
+          organization: 'Salesforce.com, Inc.',
+          designation: 'Software Developer - Intern',
+          role: 'Salesforce Cloud Developer - BizTech Org',
+          period: 'May\'19 - Aug\'19'
+        },
+        event:{
+          details: [
+            'Contributed is developing software modules and implementing spring stories for the Salesforce in-house Real Estate ' + 
+            'Management System (REMS).',
+            'Achieved the title for a Trailhead Ranger.'
+          ]
+        }
+      }
+    );
 
-    // this.profExpEvents.unshift(
-    //   {
-    //     eventLogoURL: '',
-    //     eventHeader:{
-    //       organization: '',
-    //       designation: '',
-    //       role: '',
-    //       period: ''
-    //     },
-    //     event:{
-    //       details: []
-    //     }
-    //   }
-    // );
-
-    // this.profExpEvents.push(
-    //   {
-    //     eventLogoUrl: '',
-    //     eventHeader:{
-    //       designation: '',
-    //       organization: '',
-    //       role: '',
-    //       period: ''
-    //     },
-    //     event:{
-    //       details: []
-    //     }
-    //   }
-    // );
-
-
-
-    // this.profExpEvents.push({
-    //   eventHeader : {
-    //     time: '',
-    //     designation: ''
-    //   },
-    //   event : {
-    //     title: ``,
-    //     desc: [
-    //       '',
-          
-    //     ]
-    //   },
-    //   eventLogoUrl: ''
-    // });
-
-    // this.profExpEvents.push({
-    //   eventHeader : {
-    //     time: 'Jan\'16 - Jul\'18',
-    //     designation: 'A.T.G. Developer, E-Commerce - Retail & C.P.G.'
-    //   },
-    //   event : {
-    //     title: `ToysRUs <a href=\"#\">UK</a> & <a href=\"#\">Poland</a>`,
-    //     desc: [
-    //       'Worked as an Oracle A.T.G. Web Commerce developer at T.C.S. for the U.K. and Poland markets of ToysRUs.',
-    //       'Click <a href="#" click="alert()">here</a> for detailed responsibilities.',
-    //       'Presented with the <a href="https://drive.google.com/file/d/0B40x1VYdbWc8WGxkQXNkOC11aFE/view?usp=sharing" ' +
-    //       'target="_blank">\'iONCommitment\'</a> award acknowledging the contributions made towards the project.'
-    //     ]
-    //   },
-    //   eventLogoUrl: ''
-    // });
-
-    // this.profExpEvents.push({
-    //   eventHeader : {
-    //     time: 'Jul\'16',
-    //     designation: 'Contributor'
-    //   },
-    //   event : {
-    //     title: 'CodeVita\'16',
-    //     desc: [
-    //       'Participated as a contributor in a crowd sourcing program for CodeVita\'16, a hackathon organized by T.C.S. annually, by ' +
-    //       'creating questions/problems, optimal solutions and test cases. These questions were presented to the participants as ' +
-    //       'challenges.',
-    //       'Attained the 7th position among 100+ participants considering the number and the complexity of the selected questions.',
-    //       'Received a <a href="https://drive.google.com/file/d/0B40x1VYdbWc8RTduS0NDcEVDeUE/view?usp=sharing" target="_blank">\'Special ' +
-    //       'Initiative\'</a> award for the same.'
-    //     ]
-    //   },
-    //   eventLogoUrl: ''
-    // });
-
-    // this.profExpEvents.push({
-    //   eventHeader : {
-    //     time: 'Dec\'15',
-    //     designation: 'Learning Enabler (L.E.)'
-    //   },
-    //   event : {
-    //     title: 'Initial Learning Program (I.L.P.)',
-    //     desc: [
-    //       'Appointed as a Learning Enabler for a period of 1 month to assist the I.L.P. faculty in training new trainees at the I.L.P. ' +
-    //       'centre.',
-    //       'Responsibilities involved conducting sessions on software development life cycle and specific programming modules, helping ' +
-    //       'trainees with difficulties in project assignments, and evaluating periodic assessments.',
-    //       'Offered the <a href="https://drive.google.com/file/d/0B40x1VYdbWc8S2pYQkpHZHBwN00/view?usp=sharing" target="_blank" > ' +
-    //       '\'Champions of I.L.P.\'</a> award.'
-    //     ]
-    //   },
-    //   eventLogoUrl: ''
-    // });
-
-    // this.profExpEvents.push({
-    //   eventHeader : {
-    //     time: 'Oct\'15 - Nov\'15',
-    //     designation: 'Trainee'
-    //   },
-    //   event : {
-    //     title: 'Initial Learning Program (I.L.P.)',
-    //     desc: [
-    //       'Attended the Initial Learning Program(I.L.P.) at TCS Trivandrum and completed an extensive training regime on .NET technology.',
-    //       'Presented a mini project to stakeholders which involved creating an E-Recruitment system in a team of 4 members.',
-    //       'Awarded the <a href="https://drive.google.com/file/d/0B40x1VYdbWc8ZnY2ZVNKVi16MGs/view?usp=sharing" target="_blank">\'Star ' +
-    //       'of the Learner\'s Group\'</a> award for securing  a top rating on completing the training.',
-    //       'Nominated by batchmates and instructors as the <a href="https://drive.google.com/file/d/0B40x1VYdbWc8bERDeXZCaGJvTFU' +
-    //       '/view?usp=sharing" target="_blank">\'L.I.R.E.L.\'</a> awardee.'
-    //     ]
-    //   },
-    //   eventLogoUrl: ''
-    // });
+    this.profExpEvents.unshift(
+      {
+        eventLogoURL: '../../assets/images/SUNYRF-logo.png',
+        eventHeader:{
+          organization: 'SUNY Research Foundation',
+          designation: 'Research Assistant (University at Buffalo)',
+          role: 'Full-Stack Developer',
+          period: 'Jan\'19 - Jan\'20'
+        },
+        event:{
+          details: [
+            'Working as a Research Assistant and a Full Stack developer for OneDataShare Project - a research project funded by NSF ' + 
+            'and IBM aimed at developing an MFT (Managed File Transfer system) to enhance and improve high-volume data sharing across ' + 
+            'different protocols.'
+          ]
+        }
+      }
+    );
    }    // constructor
 
   ngOnInit() {
